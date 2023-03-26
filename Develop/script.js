d0b5132453d9fd5a4a29fb7bf3a24575f530efe9
Andugithub <78062402+Andugithub@users.noreply.github.com>
2023-03-26 14:39:51 -0700

    update commit on the javascript file

--- Develop/script.js
+++ Develop/script.js
@@ -20,13 +20,7 @@
   };
        
   
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  
          var presentTime = $('.present');
          presentTime.text('10Am')
          var pastTime = $('.past')
@@ -35,6 +29,9 @@
          if (currentHour ==='4pm'){
           $('#hour-4').addClass('present');
           elseif(5 >= currentHour <= '12AM')
+          {
+            $('#hour-5').addClass('future');
+          }
 
          
     
